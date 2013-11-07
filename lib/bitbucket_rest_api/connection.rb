2098997cc102e8f5cf584df7d21c3303b518128c
Michael Eatherly <meatherly@gmail.com>
2013-11-07 16:20:21 -0600

    Adding the ability to use Oauth if you supply the token consumer key and secret when initializing a new instance of BitBucket.

--- lib/bitbucket_rest_api/connection.rb
+++ lib/bitbucket_rest_api/connection.rb
@@ -42,7 +42,7 @@ module BitBucket
         #builder.use BitBucket::Request::Jsonize
         builder.use Faraday::Request::Multipart
         builder.use Faraday::Request::UrlEncoded
-        builder.use FaradayMiddleware::OAuth, {:consumer_key => client_id, :consumer_secret => client_secret, :token => oauth_token, :token_secret => oauth_secret} if oauth_token? and oauth_secret?
+        builder.use FaradayMiddleware::OAuth, {:consumer_key => client_id, :consumer_secret => client_secret, :token => oauth_token, :token_secret => oauth_secret} if client_id? and client_secret?
         builder.use BitBucket::Request::BasicAuth, authentication if basic_authed?
         builder.use FaradayMiddleware::EncodeJson
 
