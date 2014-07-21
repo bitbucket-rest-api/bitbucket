d13b554e14ff64334b7373c6970b7bf6122da3be
Peter Kovacs <peter@sanebox.com>
2014-07-21 16:02:18 -0400

    Add Invitations to client.

---
+++ lib/bitbucket_rest_api/invitations.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+module BitBucket
+  class Invitations < API
+    def invite( user_name, repo_name, emailaddress, perm )
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of emailaddress
+      perm ||= 'write'
+
+      post_request( "/invitations/#{user}/#{repo.downcase}/#{emailaddress}", permission: perm )
+    end
+  end
+end
