d278db038b60b0f280703701be4e64bcb25a8c7a
Jasbir Singh <jasbirsingh279@gmail.com>
2016-03-11 18:41:55 +0000

    Add specs for #convert_to_constant

--- spec/bitbucket_rest_api/api_factory_spec.rb
+++ spec/bitbucket_rest_api/api_factory_spec.rb
@@ -21,4 +21,10 @@ describe BitBucket::ApiFactory do
       expect(factory.create_instance('User', {})).to be_kind_of(BitBucket::User)
     end
   end
+
+  context '#convert_to_constant' do
+    it 'knows how to convert nested classes' do
+      expect(factory.convert_to_constant('Repos::Changesets')).to eql(BitBucket::Repos::Changesets)
+    end
+  end
 end # BitBucket::ApiFactory
