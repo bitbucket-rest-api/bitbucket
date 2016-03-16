4fcdf46eb84fe6952ba9b5743c252ee9d6fa1ac9
Ilias Spyropoulos <ilias@heavymelon.com>
2016-03-16 20:00:29 +0200

    Renamed Repos::Sources#get to #list as it returns a list of files
    Added a new `Repos::Sources#get` method, which returns the raw
    contents of an individual file.

--- lib/bitbucket_rest_api/repos/sources.rb
+++ lib/bitbucket_rest_api/repos/sources.rb
@@ -3,27 +3,35 @@
 module BitBucket
   class Repos::Sources < API
 
-    REQUIRED_COMMENT_PARAMS = %w[
-        body
-        changeset_id
-        line
-        path
-        position
-      ].freeze
-
-    # Gets a source of repo 
+    # Gets a list of the src in a repository.
     #
     # = Examples
     #  @bitbucket = BitBucket.new
-    #  @bitbucket.repos.sources.get 'user-name', 'repo-name', '6dcb09b5b57875f334f61aebed6', 'app/contorllers/')
+    #  @bitbucket.repos.sources.list 'user-name', 'repo-name', '6dcb09b5b57875f334f61aebed6', 'app/contorllers/')
     #
-    def get(user_name, repo_name, sha, path, params={})
+    def list(user_name, repo_name, sha, path)
       _update_user_repo_params(user_name, repo_name)
       _validate_user_repo_params(user, repo) unless user? && repo?
-      _validate_presence_of sha
-      normalize! params
+      _validate_presence_of(sha, path)
 
-      get_request("/1.0/repositories/#{user}/#{repo.downcase}/src/#{sha}/#{path}", params)
+      get_request("/1.0/repositories/#{user}/#{repo.downcase}/src/#{sha}/#{path}")
+    end
+    alias :all :list
+
+    # Gets information about an individual file. This method returns the file's
+    # size and contents.  If the file is encoded, this method returns the files
+    # encoding; Currently, Bitbucket supports only base64 encoding.
+    #
+    # = Examples
+    #  @bitbucket = BitBucket.new
+    #  @bitbucket.repos.sources.get 'user-name', 'repo-name', '6dcb09b5b57875f334f61aebed6', 'app/assets/images/logo.jpg')
+    #
+    def get(user_name, repo_name, sha, path)
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of(sha, path)
+
+      get_request("/1.0/repositories/#{user}/#{repo.downcase}/raw/#{sha}/#{path}")
     end
     alias :find :get
 
