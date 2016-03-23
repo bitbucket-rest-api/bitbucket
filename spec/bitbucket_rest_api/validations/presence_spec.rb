fa711406660584447a4415bb9f49a1ea8b53f1d3
Chris Tweedie <cltweedie@gmail.com>
2016-03-23 14:05:55 +0000

    Check both user_name and repo_name are not nil in presence_spec

--- spec/bitbucket_rest_api/validations/presence_spec.rb
+++ spec/bitbucket_rest_api/validations/presence_spec.rb
@@ -5,7 +5,8 @@ describe BitBucket::Validations::Presence do
 
   describe "._validates_user_repo_params" do
     it 'raises an ArgumentError if user_name or repo_name is nil' do
-      expect{presence_checker._validate_user_repo_params('username')}.to raise_error ArgumentError
+      expect{presence_checker._validate_user_repo_params('user_name')}.to raise_error ArgumentError
+      expect{presence_checker._validate_user_repo_params(nil, 'repo_name')}.to raise_error ArgumentError
     end
   end
 end
