af16cf8a5c8cf37497fd35b92fd0f299289c420d
Chris Tweedie <cltweedie@gmail.com>
2016-03-31 11:45:08 +0100

    Implement followers in team.rb

--- lib/bitbucket_rest_api/team.rb
+++ lib/bitbucket_rest_api/team.rb
@@ -24,5 +24,11 @@ module BitBucket
       response["values"].each { |el| yield el }
     end
 
+    def followers(team_name)
+      response = get_request("/2.0/teams/#{team_name.to_s}/followers")
+      return response unless block_given?
+      response["values"].each { |el| yield el }
+    end
+
   end # Users
 end # BitBucket
