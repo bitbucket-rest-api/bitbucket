c876a0bdaa9542122ef27f2703d2944d2b9b207e
Chris Tweedie <cltweedie@gmail.com>
2016-03-31 10:54:28 +0100

    Add test for .profile to team_spec

--- spec/bitbucket_rest_api/team_spec.rb
+++ spec/bitbucket_rest_api/team_spec.rb
@@ -14,15 +14,30 @@ describe BitBucket::Team do
     end
 
     context 'without a block' do
-      it 'should send a GET request for the teams of which the user is a member' do
+      it 'sends a GET request for the teams of which the user is a member' do
         team.list(:member)
       end
     end
 
     context 'with a block' do
-      it 'should send a GET request for the teams of which the user is a member' do
+      it 'sends a GET request for the teams of which the user is a member' do
         team.list(:member) { |team| team }
       end
+    end
+  end
+
+  describe '.profile' do
+    before do
+      expect(team).to receive(:request).with(
+        :get,
+        '/2.0/teams/team_name',
+        {},
+        {}
+      )
+    end
+
+    it 'sends a GET request for the profile for the team' do
+      team.profile('team_name')
     end
   end
 end
