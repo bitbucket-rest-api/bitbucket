0e3b5f5de104c4e6e0c2ce07a979beabcfaf94f3
Richard Huang <flyerhzm@gmail.com>
2017-09-25 19:57:42 +0800

    privilege request with privilege_account_name

--- lib/bitbucket_rest_api/privileges.rb
+++ lib/bitbucket_rest_api/privileges.rb
@@ -8,11 +8,15 @@ module BitBucket
       super(options)
     end
 
-    def repo_list(user_name, repo_name)
+    def repo_list(user_name, repo_name, privilege_account = nil)
       _update_user_repo_params(user_name, repo_name)
       _validate_user_repo_params(user, repo) unless user? && repo?
 
-      get_request("/1.0/privileges/#{user_name}/#{repo_name}")
+      if privilege_account
+        get_request("/1.0/privileges/#{user_name}/#{repo_name}/#{privilege_account}")
+      else
+        get_request("/1.0/privileges/#{user_name}/#{repo_name}")
+      end
     end
   end
 end
--- spec/bitbucket_rest_api/privileges_spec.rb
+++ spec/bitbucket_rest_api/privileges_spec.rb
@@ -4,17 +4,34 @@ describe BitBucket::Privileges do
   let(:repo) { BitBucket::Privileges.new }
 
   describe '.repo_list' do
-    before do
-      expect(repo).to receive(:request).with(
-        :get,
-        '/1.0/privileges/mock_username/mock_repo',
-        {},
-        {}
-      )
+    context 'without privilege_account' do
+      before do
+        expect(repo).to receive(:request).with(
+          :get,
+          '/1.0/privileges/mock_username/mock_repo',
+          {},
+          {}
+        )
+      end
+
+      it 'should send a GET request for the given repo' do
+        repo.repo_list('mock_username', 'mock_repo')
+      end
     end
 
-    it 'should send a GET request for the given repo' do
-      repo.repo_list('mock_username', 'mock_repo')
+    context 'with privilege_account' do
+      before do
+        expect(repo).to receive(:request).with(
+          :get,
+          '/1.0/privileges/mock_username/mock_repo/mock_privilege_account',
+          {},
+          {}
+        )
+      end
+
+      it 'should send a GET request for the given repo' do
+        repo.repo_list('mock_username', 'mock_repo', 'mock_privilege_account')
+      end
     end
   end
 end
